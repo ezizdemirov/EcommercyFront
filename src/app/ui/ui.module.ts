d6d455c8838d4d98fe5f57d58e5a8b34b55e8666
Ilyas Aghakishiyev <ilyas.agakisili@mail.ru>
2023-06-30 23:53:27 +0400

    new

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
+import { LayoutModule } from './layout/layout.module';
 
 
 
@@ -8,7 +9,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    LayoutModule
   ]
 })
 export class UiModule { }
