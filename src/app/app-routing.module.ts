0cda97b5f5fd7a18dd82c67bd3e71875a5863ead
Ilyas Aghakishiyev <ilyas.agakisili@mail.ru>
2023-07-01 03:54:33 +0400

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,16 @@ import { LayoutComponent } from './admin/layout/layout.component';
 const routes: Routes = [
   {
     path: "admin", component: LayoutComponent, children: [
-    
+
       { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
       { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
       { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) },
     ]
   },
-  { path: "", component: HomeComponent, children:
-[
+  { path: "", component: HomeComponent},
+
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
-  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
-]
-},
-  
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) }
 
 ];
 
