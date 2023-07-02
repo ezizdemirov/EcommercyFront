e99d226f58827a54ebb5bb763e2120719178944a
Ilyas Aghakishiyev <ilyas.agakisili@mail.ru>
2023-07-02 18:31:47 +0400

    admin edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,47 @@ import { LayoutComponent } from './admin/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: "admin", component: LayoutComponent, children: [
-
-      { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
-      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
-      { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) },
-    ]
+    path: 'customers',
+    loadChildren: () =>
+      import('./admin/components/customer/customer.module').then(
+        (module) => module.CustomerModule
+      ),
   },
-  { path: "", component: HomeComponent},
+  {
+    path: 'adminProducts',
+    loadChildren: () =>
+      import('./admin/components/products/products.module').then(
+        (module) => module.ProductsModule
+      ),
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./admin/components/order/order.module').then(
+        (module) => module.OrderModule
+      ),
+  },
 
-  { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
-  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) }
+  { path: '', component: HomeComponent },
 
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./ui/components/baskets/baskets.module').then(
+        (module) => module.BasketsModule
+      ),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./ui/components/products/products.module').then(
+        (module) => module.ProductsModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
